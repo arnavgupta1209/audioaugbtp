0123ff8bbee7d8a799c71bfa200a2c40cb3126f4
arnavgupta1209 <arnavgupta1209@gmail.com>
2024-09-14 14:18:23 +0530

    added augmentations.py created skeletal dataset/loader

---
+++ augmentations.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import math, random
+import torch
+import torchaudio
+from torchaudio import transforms
+from IPython.display import Audio
+#from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import IPython.display as ipd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision.models as models
+
+def pad_trunc(aud, max_ms):
+    sig, sr = aud
+    num_rows, sig_len = sig.shape
+    max_len = sr//1000 * max_ms
+
+    if (sig_len > max_len):
+      # Truncate the signal to the given length
+      sig = sig[:,:max_len]
+
+    elif (sig_len < max_len):
+      # Length of padding to add at the beginning and end of the signal
+      pad_begin_len = random.randint(0, max_len - sig_len)
+      pad_end_len = max_len - sig_len - pad_begin_len
+
+      # Pad with 0s
+      pad_begin = torch.zeros((num_rows, pad_begin_len))
+      pad_end = torch.zeros((num_rows, pad_end_len))
+
+      sig = torch.cat((pad_begin, sig, pad_end), 1)
+      
+    return (sig, sr)
